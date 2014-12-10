2da9bf9105688fc908189760cd1141f143ba3423
Dennis Hoppe <github@debian-solutions.de>
2014-12-10 18:21:19 +0100

    Add a custom fact because the value of ipaddress gets messed up if you use Docker

---
+++ lib/facter/ipaddress_primary.rb
@@ -0,0 +1,9 @@
+Facter.add("ipaddress_primary") do
+  setcode do
+    if Facter.value('ipaddress_eth0')
+      Facter.value('ipaddress_eth0')
+    else
+      Facter.value('ipaddress')
+    end
+  end
+end
